1f3c0684b43d738b8d256101eaa781234c5ccf4c
キャンド <amehikoji@gmail.com>
2022-07-31 14:48:45 -0400

    fixed test so it doesn't error

--- tests/coords.rs
+++ tests/coords.rs
@@ -4,5 +4,5 @@ use Anjin::coords::Coords2D;
 fn test_coords() {
     let a = Coordinates2D::new(1., 3.);
     let b = Coordinates2D::new(5., 6.);
-    assert_eq!(a.distance_from(&b), 4.)
+    assert_eq!(a.distance_from(&b), 5.)
 }
