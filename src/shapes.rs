bcbdbdfdce3e2561eb746b28051925c86d2afded
Ch3ru09 <80120382+Ch3ru09@users.noreply.github.com>
2022-07-30 22:02:16 -0400

    coordinates intersection works

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,16 +1,32 @@
+#[derive(Debug)]
 struct Coordinates {
   x: f32,
-  y: f32,
+  y: f32
 }
 
+#[derive(Debug)]
 struct Curve {
   coords: Vec<Coordinates>
 }
 
 impl Curve {
-  fn intersects(&self) -> bool {
-    self.coords.map(|c, i| {
-      
-    })
+  pub fn intersects(&self) -> bool {
+    for i in 0..(self.coords.len()) {
+      for j in i+1..(self.coords.len()) {
+        let a: f32 = self.coords[i].x - self.coords[j].x;
+        let b: f32 = self.coords[i].y - self.coords[j].y;
+
+        if a == 0. && b == 0. {
+          return true
+        }
+      }
+    }
+    false
   }
+}
+
+fn main() {
+  let curve: Curve = {Curve {coords: vec![{Coordinates{x: 0., y: 2.}}, {Coordinates{x: 0., y: 2.}}, {Coordinates{x: 1., y: 3.}}]}};
+  
+  println!("{:?}", curve.intersects());
 }
